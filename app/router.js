7e01e4f8b1729f0cc5e3d9962e00bb1558d5a915
namrataukirde <namrata.ukirde@kiprosh.com>
2016-08-22 15:44:24 +0530

    showing individual post with dynamic segment

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('posts');
+  this.route('posts', function(){
+    this.route('post', { path: "/:post_id"});
+  });
 });
 
 export default Router;
